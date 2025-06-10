cf527420cca4459f06627b15538c6b67dd9969cd
java1777 <108736263+java1777@users.noreply.github.com>
2025-06-10 09:10:55 +0500

    end
    end

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const h1 = document.createElement('h1')
-const email = document.getElementById('email');
-const pass = document.getElementById('password');
-const btn = document.getElementById('btn');
-// const pass1=Number(pass.value)
-// if(email.value.length==0 || pass.value.length==0 || typeof pass1=='number'){
-//   alert("Son kiriting va bo'sh joy qomasin")
-// }
-const res=document.getElementById('btn');
-res.addEventListener('click',()=>{
-  const a=`Login: ${email.value}  Password: ${pass.value}`
-  h1.textContent=a
+const email = document.getElementById('mailInput');
+const pass = document.getElementById('passwordInput');
+const res = document.getElementById('btn');
+res.addEventListener('click', () => {
+  if (email.value.length === 0 || pass.value.length === 0) {
+    alert("Bo'sh joy qomasin")
+    return
+  }
+  const a = `Login: ${email.value}  Password: ${pass.value}`
+  h1.textContent = a
+  h1.style.color='red'
   document.body.appendChild(h1)
 })
