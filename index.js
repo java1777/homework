33de6157dd28c6947db7a0b1ff53a563e0ff2287
java1777 <108736263+java1777@users.noreply.github.com>
2025-06-12 16:39:46 +0500

    up

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
+const wrapperEl = document.querySelector('#wrapper');
 fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json())
-    .then(res => res.forEach(element => {
-        const lading = document.getElementById('loading')
-        const continer = document.getElementById('containerBox')
-        const box = document.createElement('div');
-        const title = document.createElement('div');
-        const id = document.createElement('div')
-        const body = document.createElement('div')
+    .then((res) => res.json())
+    .then((res) => print(res))
+    .catch((err) => console.error('Fetch error: ', err));
 
-        box.classList.add('box');
-        title.classList.add('title');
-        id.classList.add('id');
-        body.classList.add('body');
-        
-        id.textContent = `ID: ${element.id}`
-        title.textContent = `TITLE: ${element.title.slice(0, 50) }`
-        body.textContent = `BODY: ${element.body.slice(0, 50)}`
-                
-        box.appendChild(id)
-        box.appendChild(title)
-        box.appendChild(body)
-
-        continer.appendChild(box)
-        lading.style.display='none'
-    })).catch(error => {
-        console.log(`Xatolik: ${error.message}`);
-        const continer = document.getElementById('containerBox')
-        continer.textContent = `Xatolik: ${error.message}`
-    })
+function print(data) {
+    data.forEach((post) => {
+        const textEl = document.createElement('h1');
+        textEl.textContent = post.title;
+        textEl.classList.add('box');
+        wrapperEl.append(textEl);
+    });
+}
