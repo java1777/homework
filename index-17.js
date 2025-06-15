b8e1573f282d4c6756ecbf87be18bcbd61ecc61f
java1777 <108736263+java1777@users.noreply.github.com>
2025-06-15 06:40:06 +0500

    up

---
+++ index-17.js
@@ -0,0 +1,41 @@
+const productWrapper = document.getElementById('productWrapper');
+const loadMoreBtn = document.getElementById('loadMoreBtn');
+let allProducts = [];
+const initialDisplayCount = 6;
+let displayedCount = initialDisplayCount;
+
+async function fetchProducts() {
+    try {
+        const response = await fetch('https://dummyjson.com/products');
+        const data = await response.json();
+        allProducts = data.products;
+        displayProducts(allProducts.slice(0, initialDisplayCount));
+        if (allProducts.length <= initialDisplayCount) {
+            loadMoreBtn.style.display = 'none';
+        }
+    } catch (error) {
+        console.error('Error fetching products:', error);
+    }
+}
+
+function displayProducts(products) {
+    products.forEach(product => {
+        const productCard = document.createElement('div');
+        productCard.className = 'product-card';
+        productCard.innerHTML = `
+            <img src="$./BMW X6.jpg">
+            <h3>${product.title}</h3>
+            <p>Price: $${product.price}</p>
+            <p>${product.description}</p>
+        `;
+        productWrapper.appendChild(productCard);
+    });
+}
+
+loadMoreBtn.addEventListener('click', () => {
+    displayProducts(allProducts.slice(displayedCount));
+    displayedCount = allProducts.length;
+    loadMoreBtn.style.display = 'none';
+});
+
+fetchProducts();
